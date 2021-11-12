78c18990cba494873d79063327898158ada47963
Alex99-bit <alexco_99@hotmail.com>
2021-11-12 12:15:21 -0600

    ver 1.0

---
+++ src/pruebas300/Pruebas300.java
@@ -0,0 +1,14 @@
+package pruebas300;
+
+public class Pruebas300 {
+
+    
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new reproductor().setVisible(true);
+            }
+        });
+    }
+    
+}
